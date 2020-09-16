b97888ab79020fc28e337357a2cd3d5b055b923b
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-09-16 14:52:04 -0700

    Update install-cloud.sh

--- hw1/install-cloud.sh
+++ hw1/install-cloud.sh
@@ -7,8 +7,8 @@ set -o pipefail
 install_deps_dnf() {
 	echo "About to install nodejs 10"
 	sudo dnf -y module install nodejs:10
-	echo "About to install git, make, g++, python3"
-	sudo dnf -y install git make gcc-c++ python3-pip
+	echo "About to install git, make, gettext, g++, python3"
+	sudo dnf -y install git make gettext gcc-c++ python3-pip
 	echo "About to install yarn"
 	curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 	sudo dnf -y install yarn
@@ -17,8 +17,8 @@ install_deps_dnf() {
 }
 
 install_deps_ubuntu() {
-	echo "About to install git, make, curl, python3"
-	sudo apt -y install git make g++ curl python3-pip
+	echo "About to install git, make, gettext, curl, python3"
+	sudo apt -y install git make gettext g++ curl python3-pip
 	curl -sL https://nodejs.org/dist/latest-v10.x/node-v10.22.1-linux-x64.tar.xz -o node-v10.20.1-linux-x64.tar.xz
 	echo "About to install nodejs"
 	sudo tar -C /opt --no-same-owner -xf node-v10.22.1-linux-x64.tar.xz
@@ -34,7 +34,7 @@ install_deps_ubuntu() {
 }
 
 install_deps_debian() {
-	echo "About to install git, make, curl, python3"
+	echo "About to install git, make, gettext, curl, python3"
 	sudo apt -y install git make g++ curl python3-pip apt-transport-https gettext
 	curl -sL https://nodejs.org/dist/latest-v10.x/node-v10.22.1-linux-x64.tar.xz -o node-v10.20.1-linux-x64.tar.xz
 	echo "About to install nodejs"
@@ -64,7 +64,7 @@ install_deps() {
 }
 
 check_deps() {
-	for dep in git node npm yarn make g++ pip3 ; do
+	for dep in git node npm yarn make g++ msgfmt pip3 ; do
 		if ! which $dep >/dev/null 2>&1 ; then
 			return 1
 		fi
